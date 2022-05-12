05c014f99a7682f1798e9ac024c2c9af9983c603
Aleksandr <65013672+gran1um@users.noreply.github.com>
2022-05-12 11:44:20 +0300

    initial

---
+++ My Places/My Places/MainViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MainViewController.swift
+//  My Places
+//
+//  Created by Alexander Popov on 11.05.2022.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    
+    let restaurantNames = [
+    "У Декса","Чайший Восход","Дикий гриль","Вита","Зиклесс"
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        
+        return cell
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
