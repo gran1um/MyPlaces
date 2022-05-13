168e0503c2ae8f03584e870b90edb891dd4a4453
Aleksandr <65013672+gran1um@users.noreply.github.com>
2022-05-13 20:21:55 +0300

    new class and custom table view cell

---
+++ My Places/My Places/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  My Places
+//
+//  Created by Alexander Popov on 13.05.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
