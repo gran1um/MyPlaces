159d18d6654649faf4cc0397a6b8ad054aeb7b1b
Aleksandr <65013672+gran1um@users.noreply.github.com>
2022-05-24 20:58:07 +0300

    add search bar, logic and fix constraints

--- My Places/My Places/AppDelegate.swift
+++ My Places/My Places/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let config = Realm.Configuration(
+            schemaVersion: 2,
+            migrationBlock: { migration, oldSchemaVersion in
+                if oldSchemaVersion < 2 {
+                    
+                }
+            })
+        
+        Realm.Configuration.defaultConfiguration = config
+        
         return true
     }
 
